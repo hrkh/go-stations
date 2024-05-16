a22b03eefa9bee1dd50c508f734b768cb8c1c85e
hrkh <hrkhjp@gmail.com>
2024-05-16 11:19:31 +0900

    Complete 13

--- handler/todo.go
+++ handler/todo.go
@@ -24,7 +24,8 @@ func NewTODOHandler(svc *service.TODOService) *TODOHandler {
 
 // ServeHTTP implements http.Handler interface.
 func (h *TODOHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
+	switch r.Method {
+	case "POST":
 		req := &model.CreateTODORequest{}
 		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 			panic(err)
@@ -41,6 +42,26 @@ func (h *TODOHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if err := json.NewEncoder(w).Encode(res); err != nil {
+			panic(err)
+		}
+
+	case "PUT":
+		req := &model.UpdateTODORequest{}
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			panic(err)
+		}
+		if req.ID == 0 || req.Subject == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintln(w, "Bad request")
+			return
+		}
+
+		res, err := h.Update(r.Context(), req)
+		if err != nil {
+			panic(err)
+		}
+
+		if err = json.NewEncoder(w).Encode(res); err != nil {
 			panic(err)
 		}
 	}
@@ -64,8 +85,12 @@ func (h *TODOHandler) Read(ctx context.Context, req *model.ReadTODORequest) (*mo
 
 // Update handles the endpoint that updates the TODO.
 func (h *TODOHandler) Update(ctx context.Context, req *model.UpdateTODORequest) (*model.UpdateTODOResponse, error) {
-	_, _ = h.svc.UpdateTODO(ctx, 0, "", "")
-	return &model.UpdateTODOResponse{}, nil
+	t, err := h.svc.UpdateTODO(ctx, req.ID, req.Subject, req.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.UpdateTODOResponse{TODO: t}, nil
 }
 
 // Delete handles the endpoint that deletes the TODOs.
